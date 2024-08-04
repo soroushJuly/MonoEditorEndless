0eb3f4a2fe1a6b716a10a7486c454aca6d36a14d
soroushJuly <soroushdevelope@gmail.com>
2024-08-04 17:03:10 +0100

    feat: add text and text list UI

---
+++ ProjectRunnerTest/Engine/UI/Text.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoEditorEndless.Engine.UI
+{
+    internal class Text
+    {
+        private string text;
+        Vector2 position;
+
+        // The font used to display UI elements  
+        SpriteFont font;
+        // Text Color
+        Color color;
+
+        public Text(string text, Vector2 position, SpriteFont font, Color color)
+        {
+            this.position = position;
+            this.text = text;
+            this.font = font;
+            this.color = color;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.DrawString(font, text, new Vector2(position.X, position.Y), color,
+                0.0f, Vector2.Zero, 0.7f, SpriteEffects.None, 1.0f);
+        }
+
+    }
+}
---
+++ ProjectRunnerTest/Engine/UI/TextList.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+
+namespace MonoEditorEndless.Engine.UI
+{
+    // A component for selection of a button in a list
+    internal class TextList
+    {
+        List<Text> textList;
+        SpriteFont font;
+        Color color;
+        int offsetX;
+        int offsetY;
+        int count;
+        int paddings;
+
+        public event EventHandler<Button> ButtonClicked;
+        public TextList(int offsetX, int offsetY, SpriteFont font, Color color, int paddings)
+        {
+            this.offsetX = offsetX;
+            this.offsetY = offsetY;
+            this.font = font;
+            this.color = color;
+            this.paddings = paddings;
+
+            textList = new List<Text>();
+        }
+
+        public void AddText(string text)
+        {
+            textList.Add(new Text(text, new Vector2(offsetX, offsetY + textList.Count * paddings), font, color));
+        }
+        public void Update()
+        {
+        }
+        public void Clear()
+        {
+            textList.Clear();
+        }
+        public int GetCount() { return textList.Count; }
+        public void Draw(SpriteBatch _spriteBatch)
+        {
+            foreach (var button in textList)
+            {
+                button.Draw(_spriteBatch);
+            }
+        }
+
+    }
+}
