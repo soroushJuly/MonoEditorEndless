193b6e56a034ac16fa77f79b39993a7877a56947
Soroush <soroushdevelope@gmail.com>
2024-08-09 10:52:42 +0100

    feat: add const routes

---
+++ ProjectRunnerTest/Routes.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace MonoEditorEndless
+{
+    internal class Routes
+    {
+        Routes _routes;
+        public const string CONTENT_DIRECTORY = @"C:\Users\sorou\Desktop\MonoEditorEndless\ProjectRunnerTest\Content\Content.mgcb";
+        public const string FRAMEWORK_TARGET = "net6.0-windows";
+    }
+}
