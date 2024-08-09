1c312343929d6730c6b6957e3c27ad6027bc3b7b
soroushJuly <soroushdevelope@gmail.com>
2024-08-09 14:44:47 +0100

    fix: routes are now relative

--- ProjectRunnerTest/Routes.cs
+++ ProjectRunnerTest/Routes.cs
@@ -1,11 +1,14 @@
-﻿
-
+﻿/// <summary>
+/// Routes here are relative to the Debug folder
+/// </summary>
 namespace MonoEditorEndless
 {
     internal class Routes
     {
         Routes _routes;
-        public const string CONTENT_DIRECTORY = @"C:\Users\sorou\Desktop\MonoEditorEndless\ProjectRunnerTest\Content\Content.mgcb";
+        public const string CONTENT_DIRECTORY = @"..\..\..\Content\Content.mgcb";
+        public const string ROOT_DIRECTORY = @"..\..\..\..";
+        public const string SAVED_PROJECTS = @"..\..\..\Editor\Saved";
         public const string FRAMEWORK_TARGET = "net6.0-windows";
     }
 }
